fa71374fd20a779a1dfbf29254de7552dfd763b1
YumiOmoto <yumi.omoto@play.jp>
2022-06-08 09:58:28 +0900

    編集

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import React-DOM from 'react-dom';
+
+const options = [
+  { value: 'js', label: 'JavaScript' },
+  { value: 'py', label: 'Python' },
+  { value: 'rb', label: 'Ruby' },
+  { value: '', label: 'その他' },
+];
+
 
 function App() {
+  const [val, setVal] = React.useState('js');
+  const [text, setText] = React.useState('');
+
+  const handleRadioChange = e => setVal(e.target.value);
+  const handleTextChange = e => setText(e.target.value);
+
+  const getAnswer = () => {
+    return val === ''
+      ? text
+      : options.find(o => o.value === val).label;
+  };
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>好きなプログラミング言語を教えてください。</p>
+    {options.map(option => (
+    <label key={option.value}>
+      <input 
+        type="radio" 
+        value= {option.value}
+        onChange={handleRadioChange}
+        checked={val === option.value}
+      />
+      {option.label}
+    </label>
+    ))}
+    {val === '' && (
+    <p>
+      自由記入：
+      <input value={text} onChange={handleTextChange}/>
+    </p>
+    )}
+    <hr />
+    <p>回答：{getAnswer()}</p>
+    </>
   );
 }
 
 export default App;
+// const root = document.getElementById('root');
+// ReactDOM.render(<App />, root);
